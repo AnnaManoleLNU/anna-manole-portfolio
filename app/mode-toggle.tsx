0d677ae884b9c7127c1432b9b31fd9020e446d55
Anna Manole <am224wd@student.lnu.se>
2025-04-02 21:24:17 +0200

    fix theme switcher and nav bar

--- app/mode-toggle.tsx
+++ app/mode-toggle.tsx
@@ -1,43 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import * as SwitchPrimitives from "@radix-ui/react-switch";
-import { Sun, Moon } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
-  const [isChecked, setIsChecked] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    setIsChecked(theme === "dark");
-  }, [theme]); 
-
-  // Handle the theme toggle
-  const handleToggle = (checked: boolean) => {
-    setIsChecked(checked);
-    setTheme(checked ? "dark" : "light");
-  };
-
-  if (isChecked === null) return null;
 
   return (
-    <SwitchPrimitives.Root
-      className={cn(
-        "peer inline-flex h-8 w-16 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors duration-300 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-primary"
-      )}
-      onCheckedChange={handleToggle}
-      checked={isChecked}
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <SwitchPrimitives.Thumb
-        className={cn(
-          "pointer-events-none flex h-7 w-7 items-center justify-center rounded-full bg-muted ring-0 transition-transform duration-300 ease-in-out data-[state=checked]:translate-x-8 data-[state=unchecked]:translate-x-0"
-        )}
-      >
-        {isChecked ? <Moon /> : <Sun />}
-      </SwitchPrimitives.Thumb>
-    </SwitchPrimitives.Root>
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all"  />
+      )}
+    </Button>
   );
 }
