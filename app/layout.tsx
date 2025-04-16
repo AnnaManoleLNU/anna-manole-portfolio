d51feae483bc13d01667a6689fdcfcd690b2c9c1
Anna Manole <am224wd@student.lnu.se>
2025-04-16 15:04:53 +0200

    remove some padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <header className="row-start-1 flex items-center justify-between px-4 py-2">
+            <header className="row-start-1 flex items-center justify-between  py-2">
               <Navigation /> {/* Client Component handles navigation */}
               <div>
                 <ModeToggle />
