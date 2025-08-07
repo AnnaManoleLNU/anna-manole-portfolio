a077d792313220b36bb88fba478c5d7f2ac77430
Anna Manole <am224wd@student.lnu.se>
2025-08-07 15:15:40 +0200

    update cms project

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,35 +1,34 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 export const projects = [
   {
     title: "Personal Web Desktop",
     type: "Programming",
-    images: [
-      "/pwd-chat.png",
-      "/pwd-cookbook.png", 
-      "/pwd-memory.png",
-    ],
+    images: ["/pwd-chat.png", "/pwd-cookbook.png", "/pwd-memory.png"],
     technologies: ["WebComponents", "CSS", "ES6", "WebSockets", "Node.js"],
-    description: "For this project I created a personal web desktop, which includes a chat application, a cookbook and a memory game. The web desktop is built using WebComponents, CSS and ES6. The chat application uses WebSockets for real-time communication. The cookbook allows users to see recipes, while the memory game is a card matching game with a timer and score tracking.",
+    description:
+      "For this project I created a personal web desktop, which includes a chat application, a cookbook and a memory game. The web desktop is built using WebComponents, CSS and ES6. The chat application uses WebSockets for real-time communication. The cookbook allows users to see recipes, while the memory game is a card matching game with a timer and score tracking.",
     link: "/pwd",
   },
   {
-    title: "Allel with CMS",
+    title: "CMS-Driven Company Website",
     type: "Programming",
-    images: [
-      "/allel-1.png",
-      "/allel-2.png",
-      "/allel-graphic-profile.png",
+    images: ["/allel-1.png", "/allel-2.png", "/allel-graphic-profile.png"],
+    technologies: [
+      "Next.js",
+      "Tailwind CSS",
+      "TypeScript",
+      "Sanity CMS",
+      "Vercel",
     ],
-    technologies: ["Next.js", "Tailwind CSS", "TypeScript", "Sanity CMS", "DigitalOcean"],
-    description: "For this web application I worked together with my client, an electrical consulatancy, to create a new graphic profile, which included a rebranding through logotype. I have also given them the possibility to manage their own content through a CMS.",
-    link: "/allel",
+    description:
+      "This project involved building a modern, responsive company website using a headless CMS. The goal was to enable easy content management, SEO optimization, and scalability—while maintaining a professional, uniform design.",
+    link: "/cmscompany",
   },
   {
     title: "Selected Retouching Projects",
@@ -41,7 +40,8 @@ export const projects = [
       "/retouching-4.jpg",
     ],
     technologies: ["Photoshop", "Lightroom", "Canva"],
-    description: "I have worked with different retouching projects, where I have used Photoshop, Lightroom and Canva to create visually appealing images. These projects include retouching portraits, product photography and nature photography.",
+    description:
+      "I have worked with different retouching projects, where I have used Photoshop, Lightroom and Canva to create visually appealing images. These projects include retouching portraits, product photography and nature photography.",
     link: "/retouching",
   },
-]
+];
